6d4a47f86337608f55aa3feb657e91f127122838
Diogo Bernardo <diogo.bernardo.dev@gmail.com>
2023-12-28 02:22:24 +0000

    fix imports on refreshToken model, shemas or endpoint

--- src/data/schemas/subscription_lvl.py
+++ src/data/schemas/subscription_lvl.py
@@ -6,17 +6,6 @@ from uuid import UUID as UUIDType
 class SubscriptionLvlSchema(BaseModel):
     """
     A schema representing different levels of subscription.
-
-    Attributes:
-        id (UUIDType): Unique identifier for the subscription level.
-        description (str): A brief description of the subscription level.
-        price (float): The price of this level of subscription.
-        upload_size_limit (int): The limit on upload size for this subscription level.
-        storage_limit (int): The storage limit provided by this subscription level.
-        its (float): Iterations per second (inference rate limit).
-        api_key_limit (int): The limit on the number of API keys.
-        requests_hour (int): The number of requests allowed per hour.
-        watermark (bool): Indicates if watermarks are applied at this level.
     """
 
     id: UUIDType = Field(description="Unique identifier for the subscription level.")
